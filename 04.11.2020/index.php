2eaffede4971677d787269d44a3d0bfefba3c55e
Daryakotyatkina <72615748+Daryakotyatkina@users.noreply.github.com>
2020-11-05 02:21:33 +0200

    Add files via upload

---
+++ 04.11.2020/index.php
@@ -0,0 +1,13 @@
+<form action="test/life.php" method="post">
+    <label>
+        <input type="text" name="string">
+        <select name="nameFUNC" id="">
+           <option value="one">Function1</option>
+            <option value="two">Function2</option>
+        </select>
+        <button> отправить </button>
+    </label>
+</form>
+
+
+
