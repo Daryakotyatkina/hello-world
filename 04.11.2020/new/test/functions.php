4882108234f78d66be3952d9cfd6fada12b864c2
Daryakotyatkina <72615748+Daryakotyatkina@users.noreply.github.com>
2020-11-08 16:51:18 +0300

    Update functions.php

--- 04.11.2020/new/test/functions.php
+++ 04.11.2020/new/test/functions.php
@@ -9,3 +9,16 @@ function life2($a, $b)
 {
     return $a - $b;
 }
+function life3($a, $b)
+{
+    return $a + $b;
+}
+
+function life4($a, $b)
+{
+    return $a - $b;
+}
+function life5($a, $b)
+{
+    return $a - $b;
+}
