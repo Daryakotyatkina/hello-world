95ac64ceea93ab9f81b04fdba0a9bcfc0c69706e
Daryakotyatkina <72615748+Daryakotyatkina@users.noreply.github.com>
2020-11-08 17:43:06 +0300

    Update life.php

--- 04.11.2020/new/test/life.php
+++ 04.11.2020/new/test/life.php
@@ -1,11 +1,20 @@
 <?php
 require_once('functions.php');
 $a = $_POST['string'];
-$b = $_POST['string_2'];
+$b = $_POST['string'];
 
 if ($_POST["nameFUNC"] == "one") {
     echo life($a,$b);
 }
 if ($_POST["nameFUNC"] == "two") {
-    echo life2($a,$b);
+    echo life2;($a,$b);
+}
+if ($_POST["nameFUNC"] == "three") {
+    echo life3($a,$b);
+}
+if ($_POST["nameFUNC"] == "four") {
+    echo life4($a,$b);
+}
+if ($_POST["nameFUNC"] == "five") {
+    echo life5($a,$b);
 }
