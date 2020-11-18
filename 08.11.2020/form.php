eb5c8602836218824e05f6c3d3bccf586b45fc08
Daryakotyatkina <72615748+Daryakotyatkina@users.noreply.github.com>
2020-11-18 02:37:24 +0200

    Add files via upload

---
+++ 08.11.2020/form.php
@@ -0,0 +1,14 @@
+<form action="test/life.php" method="post">
+    <label>
+        <input type="text" name="string">
+        <input type="text" name="string_2">
+        <select name="nameFUNC">
+            <option value="one">Function1</option>
+            <option value="two">Function2</option>
+            <option value="three">Function3</option>
+            <option value="four">Function4</option>
+            <option value="five">Function5</option>
+        </select>
+        <button> отправить </button>
+    </label>
+</form>
---
+++ 08.11.2020/index.php
@@ -0,0 +1,23 @@
+<?php
+$pageName = $_GET['page_type'];
+include_once 'form.php';
+include_once 'data.php';
+include_once './cms_template/layout.php';
+
+
+function life($a, $b){
+    return $a + $b;
+}
+function life2($a, $b){
+    return $a - $b;
+}
+function life3($a, $b){
+    return $a + $b;
+}
+function life4($a, $b){
+    return $a - $b;
+}
+function life5($a, $b){
+    return $a - $b;
+}
+
